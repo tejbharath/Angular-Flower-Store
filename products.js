9fc66401e48f449e95383029796a224e57ac7493
Tej Bharath Mujje <tmujje@gmail.com>
2017-03-06 21:37:54 -0600

    AngularJS and BootStrap application of a simple flower store
    Used AngularJS and BootStrap

---
+++ products.js
@@ -0,0 +1,36 @@
+(function(){
+    var app = angular.module('store-directives', []);
+
+    app.directive("productDescription", function() {
+      return {
+        restrict: 'E',
+        templateUrl: "product-description.html"
+      };
+    });
+
+    app.directive("productReviews", function() {
+      return {
+        restrict: 'E',
+        templateUrl: "product-reviews.html"
+      };
+    });
+
+    app.directive("productTabs", function() {
+      return {
+        restrict: "E",
+        templateUrl: "product-tabs.html",
+        controller: function() {
+          this.tab = 1;
+
+          this.isSet = function(checkTab) {
+            return this.tab === checkTab;
+          };
+
+          this.setTab = function(activeTab) {
+            this.tab = activeTab;
+          };
+        },
+        controllerAs: "tab"
+      };
+    });
+  })();
